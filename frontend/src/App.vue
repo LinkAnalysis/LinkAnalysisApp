7bbe8602ca42c0e555a6085cec60ba25d61851b6
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-07-16 09:39:37 +0200

    GraphView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,27 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue"
+import Graph from "graphology"
+import GraphView from "./components/GraphView.vue"
+import { ref } from "vue"
+
+const graph = ref(new Graph())
+
+graph.value.addNode("n1", {
+  label: "Node 1",
+  x: 0,
+  y: 0,
+  size: 10,
+  color: "#FF5733",
+})
+graph.value.addNode("n2", {
+  label: "Node 2",
+  x: 3,
+  y: 1,
+  size: 10,
+  color: "#33C1FF",
+})
+graph.value.addEdge("n1", "n2")
 </script>
 
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png" />
-  <HelloWorld />
+  <GraphView :graph="graph" />
 </template>
-
-<style>
-#logo {
-  display: block;
-  width: 50%;
-  height: 50%;
-  margin: auto;
-  padding: 10% 0 0;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-origin: content-box;
-}
-</style>
---
+++ frontend/src/components/GraphView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Graph from "graphology"
+import Sigma from "sigma"
+import { onBeforeUnmount, onMounted, ref } from "vue"
+
+const props = defineProps({
+  graph: Graph,
+})
+const container = ref(null)
+let renderer = null
+
+onMounted(() => {
+  renderer = new Sigma(props.graph, container.value)
+})
+
+onBeforeUnmount(() => {
+  if (renderer) renderer.kill()
+})
+</script>
+
+<template>
+  <div ref="container" class="sigma-container"></div>
+</template>
+
+<style scoped>
+.sigma-container {
+  width: 500px;
+  height: 500px;
+  background: lightblue;
+}
+</style>
