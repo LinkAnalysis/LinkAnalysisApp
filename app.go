64652121c73d90ae6f058f3ad0f781013c8769db
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-07-24 11:29:52 +0200

    spectral layout calculation

--- app.go
+++ app.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"gonum.org/v1/gonum/mat"
 )
 
 // App struct
@@ -72,6 +76,85 @@ func (a *App) SaveStringToFile(path string, content string) error {
 
 func (a *App) SaveBytesToFile(path string, content []byte) error {
 	return os.WriteFile(path, content, 0644)
+}
+
+type EigenPair struct {
+	Value   float64
+	Index   int
+	RealVec []float64
+}
+
+func (a *App) SpectralLayout(laplacian map[string]map[string]float32) map[string]map[string]float64 {
+	rowKeys := make([]string, 0, len(laplacian))
+	colKeySet := make(map[string]bool)
+	for rowKey, cols := range laplacian {
+		rowKeys = append(rowKeys, rowKey)
+		for colKey := range cols {
+			colKeySet[colKey] = true
+		}
+	}
+	colKeys := make([]string, 0, len(colKeySet))
+	for k := range colKeySet {
+		colKeys = append(colKeys, k)
+	}
+	sort.Strings(rowKeys)
+	sort.Strings(colKeys)
+
+	data := make([]float64, 0, len(rowKeys)*len(colKeys))
+	for _, rowKey := range rowKeys {
+		row := laplacian[rowKey]
+		for _, colKey := range colKeys {
+			val := float64(row[colKey])
+			data = append(data, val)
+		}
+	}
+	r := len(rowKeys)
+	c := len(colKeys)
+
+	L := mat.NewDense(r, c, data)
+	var eig mat.Eigen
+	eig.Factorize(L, mat.EigenRight)
+	values := eig.Values(nil)
+	var cvecs mat.CDense
+	eig.VectorsTo(&cvecs)
+
+	pairs := make([]EigenPair, 0, len(values))
+	rows, _ := cvecs.Dims()
+
+	for i, val := range values {
+		if math.Abs(real(val)) < 1e-10 {
+			continue
+		}
+		vec := make([]float64, rows)
+		for j := range rows {
+			c := cvecs.At(j, i)
+			vec[j] = real(c)
+		}
+
+		pairs = append(pairs, EigenPair{
+			Value:   math.Abs(real(val)),
+			Index:   i,
+			RealVec: vec,
+		})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].Value < pairs[j].Value
+	})
+
+	vec2 := pairs[1].RealVec
+	vec3 := pairs[2].RealVec
+
+	res := make(map[string]map[string]float64)
+
+	for i := range len(vec2) {
+		res[strconv.Itoa(i)] = map[string]float64{
+			"x": vec2[i],
+			"y": vec3[i],
+		}
+	}
+
+	return res
 }
 
 // startup is called when the app starts. The context is saved
