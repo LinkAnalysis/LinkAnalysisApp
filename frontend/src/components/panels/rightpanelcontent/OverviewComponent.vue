1564ef939c54ff96dd8b62a26d02cd645e0fce1a
unknown <bieniaszkacper03@gmail.com>
2025-07-25 14:42:02 +0200

    added hiding nodes

--- frontend/src/components/panels/rightpanelcontent/OverviewComponent.vue
+++ frontend/src/components/panels/rightpanelcontent/OverviewComponent.vue
@@ -1,45 +1,36 @@
-<script setup>
-import { useI18n } from "vue-i18n"
-import { storeToRefs } from "pinia"
-import { useTabsStore } from "../../../stores/tabsStore"
-
-const props = defineProps({
-  nodeCount: Number,
-  edgesCount: Number,
-})
-
-const { t } = useI18n()
-const graphType = "undirected"
-
-const tabsStore = useTabsStore()
-const { getSelectedNodeFileName, getSelectedEdgeFileName } =
-  storeToRefs(tabsStore)
-const nodeFileName = getSelectedNodeFileName
-const edgeFileName = getSelectedEdgeFileName
-</script>
-
 <template>
   <div class="panel-section">
     <h2>{{ t("overview.title") }}</h2>
+
     <div class="table-wrapper">
       <table>
         <tbody>
           <tr>
             <td>{{ t("overview.num_of_edges") }}</td>
-            <td>{{ edgesCount }}</td>
+            <td>
+              {{ totalEdges }}
+              ({{ visibleEdges }} / {{ hiddenEdges }})
+            </td>
           </tr>
+
           <tr>
             <td>{{ t("overview.num_of_nodes") }}</td>
-            <td>{{ nodeCount }}</td>
+            <td>
+              {{ totalNodes }}
+              ({{ visibleNodes }} / {{ hiddenNodes }})
+            </td>
           </tr>
+
           <tr>
             <td>{{ t("overview.graph") }}</td>
             <td>{{ t(`overview.graph_type.${graphType}`) }}</td>
           </tr>
+
           <tr>
             <td>{{ t("overview.edge_file") }}</td>
             <td>{{ edgeFileName || t("overview.no_file_selected") }}</td>
           </tr>
+
           <tr>
             <td>{{ t("overview.node_file") }}</td>
             <td>{{ nodeFileName || t("overview.no_file_selected") }}</td>
@@ -49,6 +40,36 @@ const edgeFileName = getSelectedEdgeFileName
     </div>
   </div>
 </template>
+
+<script setup>
+import { useI18n } from "vue-i18n"
+import { storeToRefs } from "pinia"
+import { computed } from "vue"
+import { useTabsStore } from "@/stores/tabsStore"
+
+const props = defineProps({
+  nodes: { type: Array, default: () => [] },
+  edges: { type: Array, default: () => [] },
+})
+
+const { t } = useI18n()
+const graphType = "undirected"
+
+const totalNodes = computed(() => props.nodes.length)
+const hiddenNodes = computed(() => props.nodes.filter(n => n.hidden).length)
+const visibleNodes = computed(() => totalNodes.value - hiddenNodes.value)
+
+const totalEdges = computed(() => props.edges.length)
+const hiddenEdges = computed(() => props.edges.filter(e => e.hidden).length)
+const visibleEdges = computed(() => totalEdges.value - hiddenEdges.value)
+
+const tabsStore = useTabsStore()
+const { getSelectedNodeFileName, getSelectedEdgeFileName } =
+  storeToRefs(tabsStore)
+
+const nodeFileName = getSelectedNodeFileName
+const edgeFileName = getSelectedEdgeFileName
+</script>
 
 <style scoped>
 .panel-section {
@@ -66,19 +87,16 @@ const edgeFileName = getSelectedEdgeFileName
   height: 8px;
 }
 table {
-  border-collapse: collapse;
-  margin: 0 auto;
   width: 100%;
+  border-collapse: collapse;
 }
-
 td {
   border: 1px solid black;
   padding: 8px;
   text-align: left;
 }
 h2 {
-  margin-top: 0;
+  margin: 0 0 8px;
   text-align: center;
-  margin-bottom: 8px;
 }
 </style>
